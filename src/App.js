fe67cc66fb567de3731ba43f507a09931b779b74
wodahs-etihw <kushwaharitesh042@gmail.com>
2024-11-06 02:41:12 +0530

    Config File Updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://q-kart-frontend-efwl.onrender.com/api/v1`,
 };
 
 function App() {
